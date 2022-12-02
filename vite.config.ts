a11df9e8e96db4e916f53acf46cf68ea4ec838ff
elmehdi rhindi <g6handi@gmail.com>
2022-12-02 13:13:51 +0100

    vite lib config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.ts'),
+      name: 'MyLib',
+      fileName: 'my-lib'
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'react',
+        }
+      }
+    }
+  }
 })
